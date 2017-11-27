496ab503374485f42f118bc556c7c4fdcf14c16b
Daniele Guido <gui.daniele@gmail.com>
2017-11-27 15:37:42 +0100

    add pipeline script from google spreadsheet to jekyll YAML

---
+++ scripts/documents.js
@@ -0,0 +1,168 @@
+/**/
+const fs          = require('fs'),
+      db          = require('diskdb'),
+      ns          = require('nested-structure'),
+      clc         = require('cli-color'),
+      YAML        = require('yamljs'),
+      async       = require('async'),
+      request     = require('request'),
+      settings    = require('./settings.js'),
+      credentials = require(settings.google_spreadsheet_to_json.credentials),
+      
+      gsjson = require('google-spreadsheet-to-json');
+
+
+console.log('Loading Google spreadsheet GID:', settings.google_spreadsheet_to_json.documents.gid);
+
+db.connect(settings.diskdb.path, ['records']);
+
+console.log(db.records.count());
+
+let c = 0;
+
+const _cy = clc.cyanBright,
+      _ye = clc.yellowBright,
+      _bl = clc.blackBright,
+      _gr = clc.greenBright.bold;
+
+
+async.waterfall([
+
+  // load data from spreadsheet.
+  (next) => {
+    c++;
+    console.log(_cy(c, '.'), _bl('load data from spreadsheet:'), settings.google_spreadsheet_to_json.documents.gid);
+    gsjson({
+      spreadsheetId: settings.google_spreadsheet_to_json.documents.gid,
+      credentials: credentials,
+      allWorksheets: true
+    }).then(res => {
+      console.log(_gr('    v'), _bl('request success!\n      - n.sheets:'), res.length)
+      let results = [];
+
+      res.forEach(d => {
+        results = results.concat(d);
+      });
+
+      results = results.filter(d => d.title && d.slug && d.type);
+      
+      
+      console.log(_bl('      - n. valid records:'), results.length);
+      
+      next(null, results)
+    }).catch(next);
+  },
+
+  (results, next) => {
+    c++;
+    console.log(_cy(c, '.'), _bl('merge with alread stored records'))
+    results
+      .map(d => {
+        let _d = {
+          title: d.title,
+          slug: d.slug.toLowerCase().trim(),
+          type: d.type,
+          data:{}
+        };
+        
+        if(d.url && d.url.length) {
+          _d.url = d.url.trim();
+        }
+        // format data__ fields
+        for(f in d) {
+          if(f.indexOf('data__') !== 0)
+            continue
+
+          let keys = f.split('__').join('.');
+          ns(_d).set(keys, d[f],{force: true})
+        }
+        return _d;
+      }).forEach(d => {
+        console.log(_bl('    merging:'), '/',_ye(d.slug),'/');
+        // last slug win all
+        db.records.update({
+          slug: d.slug, 
+        }, d, {
+          upsert: true
+        })
+      });
+    setImmediate(next);
+  },
+
+  (next) => {
+    c++;
+    
+    todos = db.records.find().filter(d => d.url && !d._resolved);
+
+    console.log(_cy(c, '.'), _bl('resolve url (if it is not done yet)'));
+    console.log(_bl('      - n. records todo:'), todos.length);
+      
+    let q = async.queue((record, callback) => {
+        record.data = {}
+
+        if(record.type == 'video') {
+          console.log(_bl('    type:'),_ye(record.type),_bl('- url:'), record.url);
+
+        
+          // youtube video or vimeo video, oembeddable.
+          // enrich with oembed
+          request.get('http://noembed.com/embed?url='+record.url, {
+            json:true
+          }, (err, res, body) => {
+            if(err) {
+              // ignore
+              console.log(err);
+
+            } else {
+              console.log(_gr('    v'), _bl('Noembed request success!\n      - provider_url:'), body.provider_url)
+      
+              record.data.embed = body;
+              record._resolved = true;
+              db.records.update({
+                _id: record._id
+              }, record);
+            }
+            
+            callback();
+          })
+        } else if(record.type == 'pdf') {
+          // download pdf image in cache folder and use google drive pdf viewer
+          setImmediate(callback);
+        } else if(record.type == 'image') {
+          // download image in cache folder.
+          setImmediate(callback);
+        } else {
+          console.log(_bl('    type:'),_ye(record.type),'not supported yet, skypping');
+
+          setImmediate(callback);
+        }
+      })
+      q.push(todos);
+      q.drain = next;
+    },
+
+    // data override
+    // (next) => {
+    //   let q = async.queue((result, callback) => {
+
+    //   })
+
+    //   setImmediate(next)
+    // },
+
+    (next) => {
+      console.log(_cy(c, '.'), _bl('save YAML file'));
+      fs.writeFile(settings.yaml.documents.path, YAML.stringify(db.records.find(), 4), next);
+    } 
+], (err) => {
+  if (err) throw err;
+  else{
+    console.log(_gr('done!'));
+  }
+})
+
+
+    //let datafields = results.
+  // open then write json
+  
+
