68e3421ab34ae757385a3e43276f625e9c77f186
Daniele Guido <gui.daniele@gmail.com>
2018-08-03 15:18:20 +0200

    add custom d3 implementation

---
+++ d3/index.js
@@ -0,0 +1,9 @@
+import {event, select, selectAll} from "d3-selection"
+import {transition} from "d3-transition"
+
+export {
+  event,
+  select,
+  selectAll,
+  transition
+};
---
+++ d3/rollup.config.js
@@ -0,0 +1,14 @@
+import node from "rollup-plugin-node-resolve";
+
+export default {
+  entry: "index.js",
+  output: {
+    format: "iife",
+    name: "d3",
+    file: "d3.js"
+  },
+
+
+  plugins: [node()],
+
+};
