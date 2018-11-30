008b7dfe046fb4240a3d30ff9b14612fb69c0cdf
Daniele Guido <gui.daniele@gmail.com>
2018-11-30 10:16:11 +0100

    Update moodle.runtime.js

--- assets/js/moodle.runtime.js
+++ assets/js/moodle.runtime.js
@@ -1,1 +1,10 @@
-console.log('hey it looks cool');
+document.addEventListener("DOMContentLoaded", function(e) {
+  console.log('Display content from ranke');
+  if (!window.jQuery) {
+    console.warning('cannot display content. Jquery needed on the page');
+    return;
+  }
+  console.log('loading...');
+  var j = window.jQuery;
+  j("#ranketwo-contents").text('loading...');
+});
